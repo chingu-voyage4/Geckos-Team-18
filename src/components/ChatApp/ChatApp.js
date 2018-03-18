3cce9bfc9bc3a5826ed8ca2f6e633bf6bb28ea7c
cadmoral <cams_00@hotmail.com>
2018-03-18 12:49:01 +0100

    RecentList and ActiveUser components

---
+++ src/components/ChatApp/ChatApp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import RecentList  from '../RecentList/RecentList';
+
+class ChatApp extends Component {
+    render() {
+        return (
+            <RecentList/>
+        );    
+}
+}
+
+export default ChatApp;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import ChatApp from './components/ChatApp/ChatApp';
 
-import ChatApp from './components/ChatApp';
 
-ReactDOM.render(<ChatApp />, document.getElementById('root'));
+
+ReactDOM.render(  <MuiThemeProvider> <ChatApp/>  </MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
+
+
+
