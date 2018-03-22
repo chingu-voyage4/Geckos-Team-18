179f0f7ec1484c4b81dc39a2f10cd78c09a88b41
donte hightower <dontedhightower@gmail.com>
2018-03-21 20:46:21 -0500

    first component commit

--- src/components/MessageInput/MessageInput.js
+++ src/components/MessageInput/MessageInput.js
@@ -1,33 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MuiThemePRovider, { RaisedButton } from 'material-ui';
+// This is the component that the user will type text into to 
+// submit a message. It will temporarily have two buttons, 
+// to make sure both types of messages can be displayed. A received 
+// message should align itself to the left and show up with a white
+// background. A sent message should align itself to the right of
+// the MessageWindow and have a pink background.
+
+import React, { Component } from 'react';
+import { RaisedButton, TextField } from 'material-ui';
 
 class MessageInput extends Component {
-  constructor(props) {
-    super(props);
-    const { fromMeStyle, toMeStyles } = this.styles;
-  }
-  
+
   render() {
+    const styles = {
+      fromMeStyle: {
+        backgroundColor: '#F4175A',
+        alignSelf: 'flex-end',
+        marginLeft: 10
+
+      },
+      toMeStyles: {
+        marginLeft: 10,
+        alignSelf: 'flex-end',
+        backgroundColor: '#fff'
+      }
+    } 
+    const { fromMeStyle, toMeStyles } = styles;
+
     return (
-    <form>
-      <TextField 
-        hintText='Place your message here'
-        />
-      <RaisedButton label='From Me' style={fromMeStyle} />
-      <RaisedButton label='To Me' style={toMeStyles} />
-    </form>
+    <div className='input'>
+      <form>
+        <TextField 
+          hintText='Place your message here'
+          hintStyle={{color: 'white'}}
+          />
+        <RaisedButton backgroundColor='#F4175A'label='From Me' style={fromMeStyle} />
+        <RaisedButton backgroundColor='#15DF88'label='To Me' style={toMeStyles} />
+      </form>
+    </div>
     );
   }
 
-  const styles = {
-    fromMeStyle: {
-      backgroundColor: '#F4175A'
-    },
-    toMeStyles: {
-      backgroundColor: '#fff'
-    } 
-  }
+  
 }
 
 export default MessageInput;
