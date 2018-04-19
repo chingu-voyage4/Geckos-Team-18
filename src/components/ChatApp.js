a0d5803ea76bc0f3121c11c44f011bb6c9d458d2
dontehightower <dontedhightower@gmail.com>
2018-04-18 22:58:16 -0500

    change import statements

---
+++ src/components/ChatApp.js
@@ -0,0 +1,43 @@
+// TOP LEVEL COMPONENT
+
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import Footer from './Footer/Footer.js';
+import Navbar from './Navbar/Navbar.js';
+import MessageWindow from './MessageWindow/MessageWindow';
+import RecentList  from './RecentList/RecentList';
+
+// A couple of adjustments to begin generating a custom theme for
+// our project. There are many other components that can be styled
+// as a part of a custom theme. canvasColor change was necessary to
+// set the default background color of most imported Material-UI
+// components. Text color must be white to show up on dark backgrounds.
+
+const muiTheme = getMuiTheme({
+  palette: {
+    canvasColor: '#0F143A',
+    textColor: 'white'
+  }
+});
+
+class ChatApp extends Component {
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme} >
+        <div
+          style={{display: 'flex', flexDirection: 'column', flexWrap: 'wrap',
+                  height: '100vh', justifyContent: 'center', margin: '0'}}
+        >
+          <Navbar />
+          <RecentList />
+          <MessageWindow />
+          <Footer />
+        </div>
+      </ MuiThemeProvider>
+    );
+  }
+}
+
+export default ChatApp;
