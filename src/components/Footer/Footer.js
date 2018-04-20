042a77c3f16f4acc83b0aa9baa1d2c594d93eab1
dontehightower <dontedhightower@gmail.com>
2018-04-20 10:42:06 -0500

    change name tooltips

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-import "./Footer.css";
+import './Footer.css';
 import IconButton from 'material-ui/IconButton';
 import Paper from 'material-ui/Paper';
 
-// The following imports were initially required, but not used within the components
-  // import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
-  // import FontIcon from 'material-ui/FontIcon';
-  // import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
-
 class Footer extends Component {
-
   render() {
     const style = {
       height: 'auto',
@@ -17,24 +11,24 @@ class Footer extends Component {
       margin: 0,
       textAlign: 'center'
     };
-    //Name placeholders TODO
+    // Name placeholders TODO
     return (
       <div className='footer'>
         <Paper style={style} zDepth={1}>
           <IconButton
-            iconClassName="fab fa-github" tooltip="sim" iconStyle={{color: 'white'}}
+            iconClassName="fab fa-github" tooltip="Donte" iconStyle={{color: 'white'}}
             tooltipPosition="top"
           />
           <IconButton
-            iconClassName="fab fa-github" tooltip="immittant" iconStyle={{ color: 'white' }}
+            iconClassName="fab fa-github" tooltip="Marcus" iconStyle={{ color: 'white' }}
             tooltipPosition="top"
           />
           <IconButton
-            iconClassName="fab fa-github" tooltip="argumenti" iconStyle={{ color: 'white' }}
+            iconClassName="fab fa-github" tooltip="Carla" iconStyle={{ color: 'white' }}
             tooltipPosition="top"
           />
           <IconButton
-            iconClassName="fab fa-github" tooltip="societati" iconStyle={{ color: 'white' }}
+            iconClassName="fab fa-github" tooltip="Nat" iconStyle={{ color: 'white' }}
             tooltipPosition="top"
           />
         </Paper>
