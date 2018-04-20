b1ce67d1f3bb4986bd8d6e4ace8d8e3bda08d42d
dontehightower <dontedhightower@gmail.com>
2018-04-20 15:40:36 -0500

    add code comments for documentation

--- src/components/Registration/RegistrationCard.js
+++ src/components/Registration/RegistrationCard.js
@@ -1,6 +1,13 @@
+// The RegistrationCard component will eventually be the entry point for 
+// The application. It should display a form for a user to insert a handle
+// When submitted, it should:
+    // associate that handle with a unique socket ID,
+    // open a socket for communication with the server,
+    // redirect the user to the Chat page.
+
 import React, { Component } from 'react';
 import { RaisedButton, TextField } from 'material-ui';
-
+import { Link } from 'react-router-dom';
 import './RegistrationCard.css';
 
 class RegistrationCard extends Component {
@@ -13,8 +20,12 @@ class RegistrationCard extends Component {
 
 
                 {/* NEXT ITEM */}
-                {/* When this button is clicked, store the handle and go to the index page */}
-                <RaisedButton backgroundColor='#F4175A' label='Create account' secondary={true}/>
+                {/* Store the handle for use when sending messages */}
+                {/* Associate Socket ID & handle so that any message from a unique id */}
+                {/* displays that it was sent from the associated handle */}
+                <Link to='/'>
+                    <RaisedButton backgroundColor='#F4175A' label='Create account' secondary={true}/>
+                </Link>
             </div>
         );
     }
