0d58fa80896a6e9767f96605e222d5d459eda482
dontehightower <dontedhightower@gmail.com>
2018-04-20 21:37:47 -0500

    create action functions

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+import * as types from './types.js';
+
+let nextMessageId = 0;
+let nextUserId = 0;
+
+export const addMessage = (message, author) => ({
+  type: types.SEND_MESSAGE,
+  id: nextMessageId++,
+  message,
+  author
+});
+
+export const addUser = name => ({
+  type: types.USER_JOINED,
+  id: nextUserId++,
+  name
+});
+
+export const messageReceived = (message, author) => ({
+  type: types.MESSAGE_RECEIVED,
+  id: nextMessageId++,
+  message,
+  author
+});
+
+export const populateUsersList = users => ({
+  type: types.USERS_LIST,
+  users
+})
