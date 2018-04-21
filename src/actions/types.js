21f0ee5549b0bdfec9956c41030c5df84711b7c5
dontehightower <dontedhightower@gmail.com>
2018-04-20 21:38:20 -0500

    action types

---
+++ src/actions/types.js
@@ -0,0 +1,6 @@
+export const SEND_MESSAGE = 'add_message';
+export const MESSAGE_RECIEVED = 'message_received';
+export const USER_JOINED = 'user_joined';
+export const USERS_LIST = 'users_list';
+export const DISCONNECT = 'disconnect';
+export const TYPING = 'typing';
